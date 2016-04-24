1e9e78e517db32f9b1d2a49c2c314685bbf0daf8
Jacob Chesley <jacob.a.chesley@gmail.com>
2016-04-24 02:55:06 -0400

    Add Memory Leak Detection, Add ImagePanel Test and set OnClose

--- Src/GUI/MainWindow/MainWindow.h
+++ Src/GUI/MainWindow/MainWindow.h
@@ -10,7 +10,21 @@
 #endif
 
 #include "Processing\Processor\Processor.h"
-#include "Processing\ImageFileLoader\ImageFileLoader.h"
+#include "Processing\ImageHandler\ImageHandler.h"
+#include "GUI\ImageDisplay\ImagePanel\ImagePanel.h"
+
+#ifdef CHECK_MEMORY_LEAK
+#define CRTDBG_MAP_ALLOC
+#include <stdlib.h>
+#include <crtdbg.h>
+
+#ifdef _DEBUG
+#ifndef DBG_NEW
+#define DBG_NEW new ( _NORMAL_BLOCK , __FILE__ , __LINE__ )
+#define new DBG_NEW
+#endif
+#endif  // _DEBUG
+#endif
 
 /**
 	Main Window is the main display window of PhoediX.
@@ -28,12 +42,17 @@ private:
 	void ShowLoadFile(wxCommandEvent& WXUNUSED(event));
 	void SetSizeProperties();
 
+	void OnClose(wxCloseEvent& closeEvent);
+
+	wxBoxSizer * sizer;
+
 	wxMenuBar * menuBar;
 	wxMenu * menuFile;
 	wxMenu * menuView;
 	wxMenu * menuHelp;
 
 	Processor * processor;
+	ImagePanel * imagePanel;
 
 	enum MenuBar {
 		ID_SHOW_LOAD_FILE,
