0c73aa2b2f6a60e1c85705502a4bdf30ab481735
Jacob Chesley <jacob.a.chesley@gmail.com>
2016-04-24 02:56:54 -0400

    Now ImageHandler

--- Src/Processing/ImageFileLoader/ImageFileLoader.cpp
+++
@@ -1,7 +0,0 @@
-#include "ImageFileLoader.h"
-
-void ImageFileLoader::LoadImageFromFile(wxString fileName, Image * image) {
-
-	wxImage fileImage(fileName);
-	image->SetDataFrom8((int8_t*)fileImage.GetData(), fileImage.GetWidth(), fileImage.GetHeight());
-}
