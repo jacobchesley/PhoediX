608b1c0dda9b554c7dd6e8ee0d1c1c502cd5225e
Jacob Chesley <jacob.a.chesley@gmail.com>
2016-04-24 02:54:18 -0400

    Add Memory Leak Detection

--- Src/App/PhoediX.cpp
+++ Src/App/PhoediX.cpp
@@ -1,9 +1,14 @@
 #include "PhoediX.h"
 
+
 IMPLEMENT_APP(PhoediX)
 
 // 'Main program' equivalent: the program execution "starts" here
 bool PhoediX::OnInit(){
+
+#ifdef CHECK_MEMORY_LEAK
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+#endif
 
     if (!wxApp::OnInit()){
 		return false;
--- Src/App/PhoediX.h
+++ Src/App/PhoediX.h
@@ -11,6 +11,19 @@
 
 #include "GUI\MainWindow\MainWindow.h"
 
+#ifdef CHECK_MEMORY_LEAK
+#define CRTDBG_MAP_ALLOC
+#include <stdlib.h>
+#include <crtdbg.h>
+
+#ifdef _DEBUG
+#ifndef DBG_NEW
+#define DBG_NEW new ( _NORMAL_BLOCK , __FILE__ , __LINE__ )
+#define new DBG_NEW
+#endif
+#endif  // _DEBUG
+#endif
+
 class PhoediX : public wxApp {
 public:
 
