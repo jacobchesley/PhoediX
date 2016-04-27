856dd3589d70bfc73769b60890492ec4d261cdf5
Jacob Chesley <jacob.a.chesley@gmail.com>
2016-04-27 17:48:21 -0400

    Move memory leak detection to new file

--- Src/Processing/Processor/Processor.h
+++ Src/Processing/Processor/Processor.h
@@ -2,19 +2,7 @@
 #define PROCESSOR_H
 
 #include "Processing\Image\Image.h"
-
-#ifdef CHECK_MEMORY_LEAK
-#define CRTDBG_MAP_ALLOC
-#include <stdlib.h>
-#include <crtdbg.h>
-
-#ifdef _DEBUG
-#ifndef DBG_NEW
-#define DBG_NEW new ( _NORMAL_BLOCK , __FILE__ , __LINE__ )
-#define new DBG_NEW
-#endif
-#endif  // _DEBUG
-#endif
+#include "Debugging\MemoryLeakCheck.h"
 
 class Processor {
 public:
