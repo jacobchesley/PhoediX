89f7144751bdebf32469b5a36571fd61fcd25c7d
Jacob Chesley <jacob.a.chesley@gmail.com>
2016-04-27 17:48:56 -0400

    Define program colors for use in all GUI elements

---
+++ Src/GUI/Colors/Colors.cpp
@@ -0,0 +1,16 @@
+#include "Colors.h"
+
+wxColor Colors::BackBlack = wxColor(0, 0, 0);
+wxColor Colors::BackDarkDarkDarkGrey = wxColor(24, 24, 24);
+wxColor Colors::BackDarkDarkGrey = wxColor(35, 35, 35);
+wxColor Colors::BackDarkGrey = wxColor(50, 50, 50);
+wxColor Colors::BackGrey = wxColor(95, 95, 95);
+wxColor Colors::BackLightGrey = wxColor(135, 135, 135);
+wxColor Colors::BackLightLightGrey = wxColor(170, 170, 170);
+wxColor Colors::BackWhite = wxColor(255, 255, 255);
+
+wxColor Colors::TextBlack = wxColor(0, 0, 0);
+wxColor Colors::TextDarkGrey = wxColor(75, 75, 75);
+wxColor Colors::TextGrey = wxColor(130, 130, 130);
+wxColor Colors::TextLightGrey = wxColor(200, 200, 200);
+wxColor Colors::TextWhite = wxColor(255, 255, 255);
---
+++ Src/GUI/Colors/Colors.h
@@ -0,0 +1,33 @@
+#ifndef COLORS_H
+#define COLORS_H
+
+// for compilers that support precompilation, includes "wx/wx.h"
+#include "wx/wxprec.h"
+
+// for all others, include the necessary headers explicitly
+#ifndef WX_PRECOMP
+#include "wx/wx.h"
+#endif
+
+#include "Debugging\MemoryLeakCheck.h"
+
+class Colors {
+public:
+	static wxColor BackBlack;
+	static wxColor BackDarkDarkDarkGrey;
+	static wxColor BackDarkDarkGrey;
+	static wxColor BackDarkGrey;
+	static wxColor BackGrey;
+	static wxColor BackLightGrey;
+	static wxColor BackLightLightGrey;
+	static wxColor BackWhite;
+	
+	static wxColor TextBlack;
+	static wxColor TextDarkGrey;
+	static wxColor TextGrey;
+	static wxColor TextLightGrey;
+	static wxColor TextWhite;
+
+};
+
+#endif
