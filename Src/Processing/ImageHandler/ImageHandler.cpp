35797898d59f5bb63bdb91b9da915b29d9ab8fd2
Jacob Chesley <jacob.a.chesley@gmail.com>
2016-04-28 12:06:04 -0400

    Add copy 16 bit image data

--- Src/Processing/ImageHandler/ImageHandler.cpp
+++ Src/Processing/ImageHandler/ImageHandler.cpp
@@ -24,3 +24,26 @@ void ImageHandler::CopyImageData8(Image * image, uint8_t * outArray) {
 		index += 1;
 	}
 }
+
+void ImageHandler::CopyImageData16(Image * image, uint16_t * outArray) {
+
+	if (image->GetColorDepth() != 16) {
+		return;
+	}
+
+	int dataSize = image->GetWidth() * image->GetHeight() * 3;
+
+	uint16_t * redData = image->Get16BitDataRed();
+	uint16_t * greenData = image->Get16BitDataGreen();
+	uint16_t * blueData = image->Get16BitDataBlue();
+
+	int index = 0;
+	for (int i = 0; i < dataSize; i += 3) {
+
+		outArray[i] = redData[index];
+		outArray[i + 1] = greenData[index];
+		outArray[i + 2] = blueData[index];
+
+		index += 1;
+	}
+}
