827261363c08ec92cad4c8e7bc555da834233225
Jacob Chesley <jacob.a.chesley@gmail.com>
2016-06-19 21:49:19 -0400

    greyscale window created

---
+++ Src/GUI/EditList/AvailableEditWindows/GreyscaleWindow/ConvertGreyscaleWindow.cpp
@@ -0,0 +1,106 @@
+#include "ConvertGreyscaleWindow.h"
+
+ConvertGreyscaleWindow::ConvertGreyscaleWindow(wxWindow * parent, wxString editName, Processor * processor) : EditWindow(parent, editName) {
+
+	this->SetBackgroundColour(parent->GetBackgroundColour());
+
+	mainSizer = new wxBoxSizer(wxVERTICAL);
+
+	// 2 Columns, 15 pixel vertical gap, 5 pixel horizontal gap
+	gridSizer = new wxFlexGridSizer(2, 15, 5);
+
+	editLabel = new wxStaticText(this, -1, editName);
+	editLabel->SetForegroundColour(Colors::TextWhite);
+	editLabel->SetFont(wxFont(13, wxFONTFAMILY_DEFAULT, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_BOLD));
+
+	greyscaleMethodLabel = new wxStaticText(this, -1, "Greyscale Conversion Method");
+	redBrightLabel = new wxStaticText(this, -1, "Red Multiplier");
+	greenBrightLabel = new wxStaticText(this, -1, "Green Multiplier");
+	blueBrightLabel = new wxStaticText(this, -1, "Blue Multiplier");
+	greyscaleMethodLabel->SetForegroundColour(Colors::TextLightGrey);
+	redBrightLabel->SetForegroundColour(Colors::TextLightGrey);
+	greenBrightLabel->SetForegroundColour(Colors::TextLightGrey);
+	blueBrightLabel->SetForegroundColour(Colors::TextLightGrey);
+
+	greyscaleMethod = new wxComboBox(this, -1);
+	greyscaleMethod->AppendString("Human Eyesight RGB Scalars");
+	greyscaleMethod->AppendString("Average RGB Values");
+	greyscaleMethod->AppendString("Custom RGB Scalars");
+	greyscaleMethod->SetSelection(0);
+	redBrightSlider = new DoubleSlider(this, (1.0 / 3.0), 0.0, 5.0, 100000);
+	greenBrightSlider = new DoubleSlider(this, (1.0 / 3.0), 0.0, 5.0, 100000);
+	blueBrightSlider = new DoubleSlider(this, (1.0 / 3.0), 0.0, 5.0, 100000);
+
+	redBrightSlider->SetValuePosition(DoubleSlider::VALUE_INLINE_RIGHT);
+	greenBrightSlider->SetValuePosition(DoubleSlider::VALUE_INLINE_RIGHT);
+	blueBrightSlider->SetValuePosition(DoubleSlider::VALUE_INLINE_RIGHT);
+
+	processButton = new wxButton(this, EditWindow::ID_PROCESS_EDITS, "Process Edits");
+
+	greyscaleMethod->SetBackgroundColour(this->GetBackgroundColour());
+	greyscaleMethod->SetForegroundColour(Colors::TextLightGrey);
+	redBrightSlider->SetForegroundColour(Colors::TextLightGrey);
+	redBrightSlider->SetBackgroundColour(parent->GetBackgroundColour());
+	greenBrightSlider->SetForegroundColour(Colors::TextLightGrey);
+	greenBrightSlider->SetBackgroundColour(parent->GetBackgroundColour());
+	blueBrightSlider->SetForegroundColour(Colors::TextLightGrey);
+	blueBrightSlider->SetBackgroundColour(parent->GetBackgroundColour());
+
+	gridSizer->Add(greyscaleMethodLabel);
+	gridSizer->Add(greyscaleMethod);
+	gridSizer->Add(redBrightLabel);
+	gridSizer->Add(redBrightSlider);
+	gridSizer->Add(greenBrightLabel);
+	gridSizer->Add(greenBrightSlider);
+	gridSizer->Add(blueBrightLabel);
+	gridSizer->Add(blueBrightSlider);
+
+	mainSizer->Add(editLabel);
+	mainSizer->AddSpacer(10);
+	mainSizer->Add(gridSizer);
+	mainSizer->AddSpacer(15);
+	mainSizer->Add(processButton, 0, wxALIGN_LEFT);
+
+	proc = processor;
+	parWindow = parent;
+
+	//this->Bind(wxEVT_SCROLL_CHANGED, (wxObjectEventFunction)&ConvertGreyscaleWindow::Process, this);
+	//this->Bind(wxEVT_TEXT_ENTER, (wxObjectEventFunction)&ConvertGreyscaleWindow::Process, this);
+	//this->Bind(wxEVT_TEXT, (wxObjectEventFunction)&ConvertGreyscaleWindow::Process, this);
+	this->Bind(wxEVT_BUTTON, (wxObjectEventFunction)&ConvertGreyscaleWindow::Process, this, EditWindow::ID_PROCESS_EDITS);
+
+	this->SetSizer(mainSizer);
+	this->FitInside();
+	this->SetScrollRate(5, 5);
+
+	this->SetClientSize(this->GetVirtualSize());
+}
+
+void ConvertGreyscaleWindow::Process(wxCommandEvent& WXUNUSED(event)) {
+
+	wxCommandEvent evt(REPROCESS_IMAGE_EVENT, ID_REPROCESS_IMAGE);
+	wxPostEvent(parWindow, evt);
+}
+
+void ConvertGreyscaleWindow::AddEditToProcessor() {
+	
+	int greyscaleSelection = greyscaleMethod->GetSelection();
+
+	if (greyscaleSelection == 0) {
+		ProcessorEdit * greyEdit = new ProcessorEdit(ProcessorEdit::EditType::CONVERT_GREYSCALE_EYE);
+		proc->AddEdit(greyEdit);
+	}
+
+	else if (greyscaleSelection == 1) {
+		ProcessorEdit * greyEdit = new ProcessorEdit(ProcessorEdit::EditType::CONVERT_GREYSCALE_AVG);
+		proc->AddEdit(greyEdit);
+	}
+
+	else if (greyscaleSelection == 2) {
+		ProcessorEdit * greyEdit = new ProcessorEdit(ProcessorEdit::EditType::CONVERT_GREYSCALE_CUSTOM);
+		greyEdit->AddParam(redBrightSlider->GetValue());
+		greyEdit->AddParam(greenBrightSlider->GetValue());
+		greyEdit->AddParam(blueBrightSlider->GetValue());
+		proc->AddEdit(greyEdit);
+	}
+}
---
+++ Src/GUI/EditList/AvailableEditWindows/GreyscaleWindow/ConvertGreyscaleWindow.h
@@ -0,0 +1,41 @@
+#ifndef CONVERT_GREYSCALE_WINDOW_H
+#define CONVERT_GREYSCALE_WINDOW_H
+
+#include "GUI\EditList\EditWindow\EditWindow.h"
+#include "GUI\Controls\DoubleSlider\DoubleSlider.h"
+
+#include "GUI\Colors\Colors.h"
+#include "Processing\Processor\Processor.h"
+#include "Debugging\MemoryLeakCheck.h"
+
+class ConvertGreyscaleWindow : public EditWindow {
+public:
+	ConvertGreyscaleWindow(wxWindow * parent, wxString editName, Processor * processor);
+	void AddEditToProcessor();
+	void Process(wxCommandEvent& WXUNUSED(event));
+
+private:
+
+	wxWindow * parWindow;
+
+	wxBoxSizer * mainSizer;
+	wxFlexGridSizer * gridSizer;
+
+	wxStaticText * editLabel;
+
+	wxStaticText * greyscaleMethodLabel;
+	wxStaticText * redBrightLabel;
+	wxStaticText * greenBrightLabel;
+	wxStaticText * blueBrightLabel;
+
+	wxComboBox * greyscaleMethod;
+	DoubleSlider * redBrightSlider;
+	DoubleSlider * greenBrightSlider;
+	DoubleSlider * blueBrightSlider;
+
+	wxButton * processButton;
+
+	Processor * proc;
+
+};
+#endif#pragma once
