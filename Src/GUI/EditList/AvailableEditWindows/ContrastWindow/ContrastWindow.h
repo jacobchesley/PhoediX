fe6e7087cf1055fb074f3bc60ef9f4e9b8641883
Jacob Chesley <jacob.a.chesley@gmail.com>
2016-06-19 21:49:04 -0400

    Contrast window created

---
+++ Src/GUI/EditList/AvailableEditWindows/ContrastWindow/ContrastWindow.h
@@ -0,0 +1,38 @@
+#ifndef CONTRAST_WINDOW_H
+#define CONTRAST_WINDOW_H
+
+#include "GUI\EditList\EditWindow\EditWindow.h"
+#include "GUI\Controls\DoubleSlider\DoubleSlider.h"
+#include "GUI\Colors\Colors.h"
+#include "Processing\Processor\Processor.h"
+#include "Debugging\MemoryLeakCheck.h"
+
+class ContrastWindow : public EditWindow {
+	public:
+		ContrastWindow(wxWindow * parent, wxString editName, Processor * processor);
+		void AddEditToProcessor();
+		void Process(wxCommandEvent& WXUNUSED(event));
+
+	private:
+
+		wxWindow * parWindow;
+
+		wxBoxSizer * mainSizer;
+		wxFlexGridSizer * gridSizer;
+
+		wxStaticText * editLabel;
+
+		wxStaticText * allContrastLabel;
+		wxStaticText * redContrastLabel;
+		wxStaticText * greenContrastLabel;
+		wxStaticText * blueContrastLabel;
+
+		DoubleSlider * allContrastSlider;
+		DoubleSlider * redContrastSlider;
+		DoubleSlider * greenContrastSlider;
+		DoubleSlider * blueContrastSlider;
+
+		wxButton * processButton;
+		Processor * proc;
+};
+#endif
