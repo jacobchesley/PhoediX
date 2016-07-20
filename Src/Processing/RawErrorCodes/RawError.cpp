57f7cbf22d207d1215489320967c9a5965ded390
Jacob Chesley <jacob.a.chesley@gmail.com>
2016-07-19 22:53:15 -0400

    Raw error codes to text

---
+++ Src/Processing/RawErrorCodes/RawError.cpp
@@ -0,0 +1,21 @@
+#include "RawError.h"
+
+wxString RawError::GetStringFromError(int librawErrorCode){
+	switch(librawErrorCode){
+		case LIBRAW_UNSUFFICIENT_MEMORY:
+			return "Libraw Error: Attempt to get memory from the system has failed.";
+
+		case LIBRAW_DATA_ERROR:
+			return "Libraw Error: A fatal error emerged during data unpacking.";
+
+		case LIBRAW_IO_ERROR:
+			return "Libraw Error: A fatal error emerged during file reading (premature end-of-file encountered or file is corrupt).";
+
+		case LIBRAW_CANCELLED_BY_CALLBACK:
+			return "Libraw Error: Processing cancelled due to calling application demand.";
+
+		case LIBRAW_BAD_CROP:
+			return "Libraw Error: Incorrect cropping coordinates were set.";
+
+	}
+}
---
+++ Src/Processing/RawErrorCodes/RawError.h
@@ -0,0 +1,22 @@
+#ifndef RAW_ERROR_H
+#define RAW_ERROR_H
+
+// for compilers that support precompilation, includes "wx/wx.h"
+#include "wx/wxprec.h"
+
+// for all others, include the necessary headers explicitly
+#ifndef WX_PRECOMP
+	#include "wx/wx.h"
+#endif
+
+
+#include "libraw.h"
+
+class RawError {
+
+public:
+	static wxString GetStringFromError(int librawErrorCode);
+private:
+
+};
+#endif
