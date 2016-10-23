06fa0ba26ae6bbfb9832713f215fe9a2117325fb
Jacob Chesley <jacob.a.chesley@gmail.com>
2016-10-23 00:48:23 -0400

    Initial commit of zoom image frame

---
+++ Src/GUI/ImageDisplay/ZoomImageFrame/ZoomImageFrame.cpp
@@ -0,0 +1,37 @@
+#include "ZoomImageFrame.h"
+
+ZoomImageFrame::ZoomImageFrame(wxWindow * parent, wxString title) : wxFrame(parent, -1, title){
+	this->SetBackgroundColour(parent->GetBackgroundColour());
+	mainSizer = new wxBoxSizer(wxVERTICAL);
+	this->SetSizer(mainSizer);
+
+	imageDisplay = new ZoomImagePanel(this);
+	this->GetSizer()->Add(imageDisplay, 0, wxEXPAND);
+	this->GetSizer()->Layout();
+
+	imageDisplay->FitImage();
+}
+
+ZoomImageFrame::ZoomImageFrame(wxWindow * parent, wxString title, wxImage * image) : wxFrame(parent, -1, title){
+	this->SetBackgroundColour(parent->GetBackgroundColour());
+	mainSizer = new wxBoxSizer(wxVERTICAL);
+	this->SetSizer(mainSizer);
+
+	imageDisplay = new ZoomImagePanel(this, image);
+	this->GetSizer()->Add(imageDisplay, 1, wxEXPAND);
+	this->GetSizer()->Layout();
+
+	imageDisplay->FitImage();
+}
+
+ZoomImageFrame::ZoomImageFrame(wxWindow * parent, wxString title, Image * image) : wxFrame(parent, -1, title){
+	this->SetBackgroundColour(parent->GetBackgroundColour());
+	mainSizer = new wxBoxSizer(wxVERTICAL);
+	this->SetSizer(mainSizer);
+
+	imageDisplay = new ZoomImagePanel(this, image);
+	this->GetSizer()->Add(imageDisplay, 0, wxEXPAND);
+	this->GetSizer()->Layout();
+
+	imageDisplay->FitImage();
+}
---
+++ Src/GUI/ImageDisplay/ZoomImageFrame/ZoomImageFrame.h
@@ -0,0 +1,25 @@
+#ifndef ZOOM_IMAGE_FRAME_H
+#define ZOOM_IMAGE_FRAME_H
+
+// for compilers that support precompilation, includes "wx/wx.h"
+#include "wx/wxprec.h"
+
+// for all others, include the necessary headers explicitly
+#ifndef WX_PRECOMP
+#include "wx/wx.h"
+#endif
+
+#include "GUI\ImageDisplay\ZoomImagePanel\ZoomImagePanel.h"
+
+class ZoomImageFrame : public wxFrame {
+public:
+	ZoomImageFrame(wxWindow * parent, wxString title);
+	ZoomImageFrame(wxWindow * parent, wxString title, Image * img);
+	ZoomImageFrame(wxWindow * parent, wxString title, wxImage * img);
+
+private:
+
+	wxBoxSizer * mainSizer;
+	ZoomImagePanel * imageDisplay;
+};
+#endif
