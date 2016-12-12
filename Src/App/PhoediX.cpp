70d967ca09923a288eb7c0bf64ecffb1a5e9b3da
Jacob Chesley <jacob.a.chesley@gmail.com>
2016-12-11 23:05:53 -0500

    Pass app for yield call during closing

--- Src/App/PhoediX.cpp
+++ Src/App/PhoediX.cpp
@@ -19,11 +19,11 @@ bool PhoediX::OnInit(){
 	//Icons icons;
 	//wxBitmap splashBitmap(icons.SplashBackground);
 
-	//wxSplashScreen* splash = new wxSplashScreen(splashBitmap, wxSPLASH_CENTRE_ON_SCREEN | wxSPLASH_TIMEOUT, 6000, NULL, -1, wxDefaultPosition, wxDefaultSize,wxBORDER_SIMPLE | wxSTAY_ON_TOP);
+	//wxSplashScreen* splash = new wxSplashScreen(splashBitmap, wxSPLASH_CENTRE_ON_SCREEN | wxSPLASH_TIMEOUT, 4000, NULL, -1, wxDefaultPosition, wxDefaultSize,wxBORDER_SIMPLE | wxSTAY_ON_TOP);
 	//wxYield();
    
     // Create and show the main window
-	MainWindow * mainWindow = new MainWindow();
+	MainWindow * mainWindow = new MainWindow(this);
 	mainWindow->Show();
     return true;
 }
