f679decac83774a0652d34f68bfe5b3777fbdfe1
Jacob Chesley <jacob.a.chesley@gmail.com>
2016-12-18 15:56:40 -0500

    Linux and macOS setup scripts

---
+++ Setup/Linux_macOS/SetupLibraw.sh
@@ -0,0 +1,4 @@
+cd /home/jacob/Development/LibRaw-0.18.0-Beta2
+
+./configure --prefix="$(pwd)" --disable-static &&
+make
---
+++ Setup/Linux_macOS/SetupLibtiff.sh
@@ -0,0 +1,4 @@
+cd /home/jacob/Development/tiff-4.0.7
+
+./configure --prefix="$(pwd)" --disable-static &&
+make
