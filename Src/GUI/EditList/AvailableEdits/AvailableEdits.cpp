155427e54aa81b5fa9740c2b4c7c93604097208e
Jacob Chesley <jacob.a.chesley@gmail.com>
2018-09-16 00:05:09 -0400

    Alphabetize the order of edits, and conform descriptions.

--- Src/GUI/EditList/AvailableEdits/AvailableEdits.cpp
+++ Src/GUI/EditList/AvailableEdits/AvailableEdits.cpp
@@ -24,21 +24,21 @@ wxString Edit::GetDescription() {
 
 
 AvailableEdits::AvailableEdits() {
-	editVector.push_back(Edit(AvailableEditIDS::EDIT_ID_ADJUST_BRIGHTNESS, "Adjust Brightness", "Adjust Image Brightness"));
-	editVector.push_back(Edit(AvailableEditIDS::EDIT_ID_CONTRAST, "Adjust Contrast", "Adjust the images contrast, to give a more vivid or flat look."));
+	editVector.push_back(Edit(AvailableEditIDS::EDIT_ID_ADJUST_BRIGHTNESS, "Adjust Brightness", "Adjust image brightness."));
+	editVector.push_back(Edit(AvailableEditIDS::EDIT_ID_CONTRAST, "Adjust Contrast", "Adjust image contrast."));
 	editVector.push_back(Edit(AvailableEditIDS::EDIT_ID_ADJUST_HSL, "Adjust HSL", "Scale and shift HSL image channels."));
 	editVector.push_back(Edit(AvailableEditIDS::EDIT_ID_ADJUST_LAB, "Adjust LAB", "Scale and shift LAB image channels."));
 	editVector.push_back(Edit(AvailableEditIDS::EDIT_ID_ADJUST_RGB, "Adjust RGB", "Shift RGB image channels."));
-	editVector.push_back(Edit(AvailableEditIDS::EDIT_ID_HSL_CURVES, "HSL Curves", "Curves tool in HSL Color Space"));
-	editVector.push_back(Edit(AvailableEditIDS::EDIT_ID_LAB_CURVES, "LAB Curves", "Curves tool in LAB Color Space"));
-	editVector.push_back(Edit(AvailableEditIDS::EDIT_ID_RGB_CURVES, "RGB Curves", "Curves tool in RGB Color Space"));
-	editVector.push_back(Edit(AvailableEditIDS::EDIT_ID_GREYSCALE, "Convert to Greyscale", "Convert the image to greyscale, choosing one of three defined methods of doing so."));
-	editVector.push_back(Edit(AvailableEditIDS::EDIT_ID_CHANNEL_MIXER, "Channel Mixer", "RGB Channel Mixer"));
-	editVector.push_back(Edit(AvailableEditIDS::EDIT_ID_ROTATE, "Rotate Image", "Rotate image 90, 180, 270, or custom amount of degrees"));
-	editVector.push_back(Edit(AvailableEditIDS::EDIT_ID_MIRROR, "Mirror Image", "Mirror image horizontally or vertically"));
-	editVector.push_back(Edit(AvailableEditIDS::EDIT_ID_SCALE, "Scale Image", "Resize image to any size"));
-	editVector.push_back(Edit(AvailableEditIDS::EDIT_ID_CROP, "Crop Image", "Crop image with a starting point, along with width and height"));
-	editVector.push_back(Edit(AvailableEditIDS::EDIT_ID_BLUR, "Blur Image", "Blur image horiztontally, vertically, or all directions"));
+	editVector.push_back(Edit(AvailableEditIDS::EDIT_ID_BLUR, "Blur Image", "Blur image horiztontally, vertically, or all directions."));
+	editVector.push_back(Edit(AvailableEditIDS::EDIT_ID_CHANNEL_MIXER, "Channel Mixer", "RGB channel mixer."));
+	editVector.push_back(Edit(AvailableEditIDS::EDIT_ID_CROP, "Crop Image", "Crop image."));
+	editVector.push_back(Edit(AvailableEditIDS::EDIT_ID_HSL_CURVES, "Curves - HSL", "Curves tool in HSL Color Space."));
+	editVector.push_back(Edit(AvailableEditIDS::EDIT_ID_LAB_CURVES, "Curves - LAB", "Curves tool in LAB Color Space."));
+	editVector.push_back(Edit(AvailableEditIDS::EDIT_ID_RGB_CURVES, "Curves - RGB", "Curves tool in RGB Color Space."));
+	editVector.push_back(Edit(AvailableEditIDS::EDIT_ID_GREYSCALE, "Greyscale", "Convert image to greyscale, choosing one of three defined methods of doing so."));
+	editVector.push_back(Edit(AvailableEditIDS::EDIT_ID_MIRROR, "Mirror Image", "Mirror image horizontally or vertically."));
+	editVector.push_back(Edit(AvailableEditIDS::EDIT_ID_ROTATE, "Rotate Image", "Rotate image 90, 180, 270, or custom degree angle."));
+	editVector.push_back(Edit(AvailableEditIDS::EDIT_ID_SCALE, "Scale Image", "Resize image to any size."));
 }
 
 wxVector<Edit> AvailableEdits::GetAvailableEdits() {
